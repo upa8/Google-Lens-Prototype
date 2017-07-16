0a5e319a9a10d651dff0238a4cfdbf40553ac95e
Pratik Upacharya <pratikupacharya@gmail.com>
2017-07-16 14:54:23 +0530

    Added loading button

--- js/app.js
+++ js/app.js
@@ -1,42 +1,46 @@
 var app = angular.module('app', ['ngAnimate']);
 app.controller('homeController', function($scope) {
-    console.log('Testing');
-
-    $scope.take_snapshot = function () {
-        console.log('I am clicker');
+    startWebcam();
+    startSpeechRecognition();
+    loading(false);
+    $scope.takeSnapshot = function () {
+        takeSnapshot();
     }
 
-    Webcam.set({
-        width: 320,
-        height: 240,
-        dest_width: 640,
-        dest_height: 480,
-        image_format: 'jpeg',
-        jpeg_quality: 90
-    });
-    Webcam.attach( '#my_camera' );
-
-    if (annyang) {
-        console.log('Hi')
-        // Let's define our first command. First the text we expect, and then the function it should call
-        var commands = {
-            'who am I': function() {
-                take_snapshot();
-            }
-        };
-
-        // Add our commands to annyang
-        annyang.addCommands(commands);
-
-        // Start listening. You can call this here, or attach this call to an event, button, etc.
-        annyang.start();
-    }else{
-        console.log('error');
+    function startWebcam() {
+        Webcam.set({
+            width: 320,
+            height: 240,
+            dest_width: 640,
+            dest_height: 480,
+            image_format: 'jpeg',
+            jpeg_quality: 90
+        });
+        Webcam.attach( '#my_camera' );
     }
+    
+    function startSpeechRecognition() {
+        // Add speech recognition
+        if (annyang) {
+            var commands = {
+                'who am I': function() {
+                    takeSnapshot();
+                }
+            };
 
-
-    function take_snapshot() {
-        console.log('Tesing')
+            // Add our commands to annyang
+            annyang.addCommands(commands);
+            annyang.start();
+        }else{
+            console.log('error');
+        }
+    }
+    
+    function loading(value) {
+        $scope.loading = value;
+    }
+    function takeSnapshot() {
+        loading(true);
         // take snapshot and get image data
         Webcam.snap( function(data_uri) {
             var base64ImageData = data_uri.replace(/^data:image\/(.*);base64,/, '');
@@ -48,25 +52,23 @@ app.controller('homeController', function($scope) {
         const app = new Clarifai.App({
             apiKey: 'ec678ed6f34d47f49f2d144fd1fad086'
         });
-
         // celebrity - e466caa0619f444ab97497640cefc4dc // Clarifai.GENERAL_MODEL
         app.models.predict("e466caa0619f444ab97497640cefc4dc", {base64: image}).then(
             function(response) {
-                var result = "<ul>";
+                var result = "";
                 var dataLength = response.outputs[0].data.regions[0].data.face.identity.concepts.length;
                 var data = response.outputs[0].data.regions[0].data.face.identity.concepts;
                 for(var i =0; i< dataLength; i++){
-                    result += '<li>' +data[i].name +" probability "+ data[i].value +'<li>';
+                    result += '<h4>' +data[i].name +" probability "+ data[i].value +'</h4>';
                 }
-                result +="</ul>"
+                result +="";
+                $scope.$apply(loading(false))
                 document.getElementById('results').innerHTML = result;
             },
             function(err) {
-                console.log('Error');
+                document.getElementById('results').innerHTML = "Error in finding the results";
+                $scope.$apply(loading(false))
             }
         );
     }
-
-
-
 });
